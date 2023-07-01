a5663453cce89e7189786871f158070780cbd153
VISHNU K <86109601+iamvishnuk@users.noreply.github.com>
2023-07-01 20:18:46 +0530

    working on video call day 2

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ const io = new Server(httpServer, {
 })
 
 io.on("connection", (socket) => {
+    console.log("socket is connection established")
     // for chating
     socket.on("join-room", room => {
         socket.join(room)
@@ -43,19 +44,33 @@ io.on("connection", (socket) => {
         io.to(to).emit("call:accepted", { from: socket.id, ans })
     })
 
-    socket.on("peer:negotiationneeded",({to, offer}) => {
-        io.to(to).emit("peer:negotiationneeded",{from: socket.id, offer})
+    socket.on("peer:negotiationneeded", ({ to, offer }) => {
+        io.to(to).emit("peer:negotiationneeded", { from: socket.id, offer })
     })
 
-    socket.on("peer:nego:done",({to, ans}) => {
-        io.to(to).emit("peer:nego:final",{from: socket.id, ans})
+    socket.on("peer:nego:done", ({ to, ans }) => {
+        io.to(to).emit("peer:nego:final", { from: socket.id, ans })
     })
 
     // videCalling 2
-    socket.on("join:room",roomId => {
-        console.log(roomId)
-        socket.join(roomId)
+    socket.on("join:room", roomId => {
+        console.log("join:room")
         socket.to(roomId).emit("new:user:joined")
+    })
+    // send offer to callee
+    socket.on("send:offer", (offer, roomId) => {
+        console.log("send:offer")
+        socket.to(roomId).emit("receive:offer", offer)
+    })
+    // answer
+    socket.on("answer:created", (ans, roomId) => {
+        console.log("answer:created")
+        socket.to(roomId).emit("receive:answer", ans)
+    })
+    // on ice candidate
+    socket.on("ice:candidate", (iceCandidate, roomId) => {
+        console.log("ice: candidate")
+        socket.to(roomId).emit("receive:iceCandidate", iceCandidate)
     })
 })
 
